3c3bb14edf968a92937a7bc4d6efc7eef1371115
newyorktank <tank@cascademesh.com>
2018-03-22 11:28:51 -0700

    ToDo list in README

--- parse4goodies.py
+++ parse4goodies.py
@@ -1,24 +1,3 @@
-#################################################################
-#   We want to see how many job ads have the magic tech words.
-# 1. Reverse and search for good_words* (DONE)
-# 2. Deal with phrases (Active Directory, Amazon Web Services)  (DONE)
-# 3. Deal with variations of words (SQL, MSSQL, MySQL, NoSQL, Mongo, Maria)
-# 4. Deal with special characters C#, C++, stuff with dashes  (DONE)
-# 5. Deal with plurals
-# 6. Limit count per ad, by doing the parsing on an ad by ad basis (DONE)
-# 7. and by not counting terms that get repeated in the same ad (DONE)
-# 8. Implement inputs, and pretty up the output to a file
-# 9. Scrap not cut-n-paste (DONE)
-#10. Crawl the web (DONE)
-#11. Capture chunk 25 chars before and after. (DONE)
-#12. Create a loop at the bottom that offers user to print detail of one term.
-#13. Create commandline interface.
-#14. Refactor, look for cleanups.
-#15. Make a React front end and place on my web-site.
-#16. purge tags from description
-#17. need to create exclusions i.e. member for ember, java but not javascript
-#18. make output for list_dict_4swords pretty
-################################################################
 #Imports
 from collections import Counter
 from bs4 import BeautifulSoup as bs4
